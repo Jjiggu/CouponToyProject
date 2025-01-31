6d7ae99b5f112d13b759bf403a82fccbbb343b52
Jihun Kim <zh1229@naver.com>
2025-01-31 15:31:35 +0900

    Add - MemberSignUpRequest Dto 작성

---
+++ src/main/java/couponToy/CouponToyProject/Member/dto/MemberSignUpRequest.java
@@ -0,0 +1,26 @@
+package couponToy.CouponToyProject.Member.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberSignUpRequest {
+
+    @NotBlank(message = "Invalid email format")
+    @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+    private String email;
+
+    @NotBlank(message = "Password id required")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}")
+    private String password;
+
+    @NotBlank(message = "Username is required")
+    @Pattern(regexp = "^[a-zA-Z가-힣]{2,12}$")
+    private String name;
+
+}
