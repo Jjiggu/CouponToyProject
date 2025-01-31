efd3f60fa1ef0e73b11b5f858f4590fe058f4ac4
Jihun Kim <zh1229@naver.com>
2025-01-31 15:31:22 +0900

    Add - Member Entity

---
+++ src/main/java/couponToy/CouponToyProject/Member/model/Member.java
@@ -0,0 +1,25 @@
+package couponToy.CouponToyProject.Member.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role;
+
+
+}
