56000b97e24de1439d6864881c3c6859dbee223f
Jihun Kim <zh1229@naver.com>
2025-02-05 09:52:04 +0900

    Add - signUpMember 추가

--- src/main/java/couponToy/CouponToyProject/Member/service/MemberService.java
+++ src/main/java/couponToy/CouponToyProject/Member/service/MemberService.java
@@ -1,23 +1,25 @@
 package couponToy.CouponToyProject.Member.service;
 
 import couponToy.CouponToyProject.Member.dto.MemberSignUpRequest;
+import couponToy.CouponToyProject.Member.dto.MemberSignUpResponse;
 import couponToy.CouponToyProject.Member.model.Member;
 import couponToy.CouponToyProject.Member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
-        this.memberRepository = memberRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
 
-    public Member signUpMember(MemberSignUpRequest memberSignUpRequest) {
-        return null;
+    public MemberSignUpResponse signUpMember(MemberSignUpRequest memberSignUpRequest) {
+        Member member = memberSignUpRequest.toEntity();
+        member.encodePassword(passwordEncoder);
+
+        return MemberSignUpResponse.fromEntity(memberRepository.save(member));
     }
 
 
