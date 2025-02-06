9343e3ac612f303e61e5cd4857d69b5ebc5f91bb
Jihun Kim <zh1229@naver.com>
2025-02-06 15:47:59 +0900

    feat: Coupon 생성 구현

---
+++ src/main/java/couponToy/CouponToyProject/Coupon/service/CouponService.java
@@ -0,0 +1,24 @@
+package couponToy.CouponToyProject.Coupon.service;
+
+import couponToy.CouponToyProject.Coupon.dto.CouponCreateRequest;
+import couponToy.CouponToyProject.Coupon.dto.CouponCreateResponse;
+import couponToy.CouponToyProject.Coupon.model.Coupon;
+import couponToy.CouponToyProject.Coupon.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    @Transactional
+    public CouponCreateResponse createCoupon(CouponCreateRequest couponCreateRequest) {
+
+        Coupon coupon = couponRepository.save(couponCreateRequest.toEntity());
+
+        return CouponCreateResponse.fromEntity(coupon);
+    }
+}
