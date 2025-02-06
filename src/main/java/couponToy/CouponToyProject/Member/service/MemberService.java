85bbd841971e307999f19ea4d92309fff8f39c60
Jihun Kim <zh1229@naver.com>
2025-02-06 14:13:55 +0900

    feat: 이메일 중복 예외처리 코드 추가

--- src/main/java/couponToy/CouponToyProject/Member/service/MemberService.java
+++ src/main/java/couponToy/CouponToyProject/Member/service/MemberService.java
@@ -6,6 +6,8 @@ import couponToy.CouponToyProject.Member.dto.MemberSignUpRequest;
 import couponToy.CouponToyProject.Member.dto.MemberSignUpResponse;
 import couponToy.CouponToyProject.Member.model.Member;
 import couponToy.CouponToyProject.Member.repository.MemberRepository;
+import couponToy.CouponToyProject.global.constant.ErrorCode;
+import couponToy.CouponToyProject.global.exception.DuplicateMemberException;
 import couponToy.CouponToyProject.global.security.JwtService;
 import couponToy.CouponToyProject.global.security.MemberDetails;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +30,8 @@ public class MemberService {
 
     @Transactional
     public MemberSignUpResponse signUpMember (MemberSignUpRequest memberSignUpRequest) {
+        validateEmailDuplicated(memberSignUpRequest);
+
         Member member = memberSignUpRequest.toEntity();
         member.encodePassword(passwordEncoder);
 
@@ -48,4 +52,11 @@ public class MemberService {
         return MemberLogInResponse.fromEntity(member, accessToken);
     }
 
+
+    private void validateEmailDuplicated(MemberSignUpRequest memberSignUpRequest) {
+        memberRepository.findByEmail(memberSignUpRequest.getEmail()).ifPresent(member -> {
+            throw new DuplicateMemberException(ErrorCode.DUPLICATE_EMAIL);
+            }
+        );
+    }
 }
