c4998ba145cde8bd3193bcddd8dd29e2734f139a
Jihun Kim <zh1229@naver.com>
2025-02-06 13:01:29 +0900

    feat: ApiUtils 추가

---
+++ src/main/java/couponToy/CouponToyProject/global/api/ApiResult.java
@@ -0,0 +1,23 @@
+package couponToy.CouponToyProject.global.api;
+
+public class ApiResult<T> {
+
+    private final boolean success;
+    private final T response;
+    private final ApiError error;
+
+    public ApiResult(boolean success, T response, ApiError error) {
+        this.success = success;
+        this.response = response;
+        this.error = error;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+
+}
