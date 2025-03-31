3980cefc8d6c76a22d2e60ba9a6e32cdedd25cb1
Jihun Kim <zh1229@naver.com>
2025-03-31 18:08:21 +0900

    Refactor: 삭제 BaseBaseTimeEntity 상속

--- src/main/java/couponToy/CouponToyProject/Coupon/dto/CouponCreateRequest.java
+++ src/main/java/couponToy/CouponToyProject/Coupon/dto/CouponCreateRequest.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CouponCreateRequest extends BaseTimeEntity {
+public class CouponCreateRequest {
 
     @NotNull
     private String name;
