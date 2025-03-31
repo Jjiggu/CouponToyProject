e4bddc584a6e9091b64b75adc1d2414093aea73d
Jihun Kim <zh1229@naver.com>
2025-03-31 17:58:58 +0900

    Refactor: 제거 중복된 errorCode 필드

--- src/main/java/couponToy/CouponToyProject/global/exception/MemberNotFoundException.java
+++ src/main/java/couponToy/CouponToyProject/global/exception/MemberNotFoundException.java
@@ -4,10 +4,7 @@ import couponToy.CouponToyProject.global.constant.ErrorCode;
 
 public class MemberNotFoundException extends BaseException{
 
-    private final ErrorCode errorCode;
-
     public MemberNotFoundException(ErrorCode errorCode) {
         super(errorCode);
-        this.errorCode = errorCode;
     }
 }
